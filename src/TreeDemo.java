c7008d53e36e52d4146278b1042df08fc04fdb4c
Will <w.stolton@gmail.com>
2020-01-30 00:47:54 +0000

    tewgwrgwtreeapp

--- src/TreeDemo.java
+++ src/TreeDemo.java
@@ -1,7 +1,6 @@
-import java.util.List;
 import java.util.Scanner;
 
-public class TreeDemo{
+public class TreeDemo {
 
     private static Node root;		//creating pointer to first node in the tree
     private static int size;
@@ -12,11 +11,9 @@ public class TreeDemo{
         String name;
         Node left;
         Node right;
-        Node parent;
 
-        public Node(String name, Node left, Node right, Node parent){
+        public Node(String name, Node left, Node right){
             this.name = name;
-            this.parent = parent;
             this.left = left;
             this.right = right;
         }
@@ -25,9 +22,6 @@ public class TreeDemo{
             return name;
         }
 
-        public Node getParent() {
-            return parent;
-        }
     }
 
     private int size(){
@@ -37,55 +31,97 @@ public class TreeDemo{
     //working recursively, we don't need left/right as the code will determine which way to go
 
 
-        //check if root, insert it if there isn't a root node as root.
-        //compare value size to size of node currently there	greater than, less than.
-        //continue down until you have a space.
+    //check if root, insert it if there isn't a root node as root.
+    //compare value size to size of node currently there	greater than, less than.
+    //continue down until you have a space.
 
 
     //say we don't want duplicates...		work recursively
-    private static boolean checkValue(String value){
-
+    private static boolean checkVal(Node current, String value){
+        if(current == null){
+            return true;
+        }
+        if(value.equals(current.name)) {
+            return false;
+        }
+        else if(value != current.name){
+            checkVal(current.left, value);
+            checkVal(current.right, value);
+        }
         return true;
     }
 
-    private static void insert(String name) {
-        if(root.left == null){
+    private static boolean checkValue(String value){
+        return checkVal(root(), value);
+    }
 
+    private static Node insert(Node current, String name) {
+        if(current == null){
+            return new Node(name, null, null);
         }
+
+        //compareTo compares strings lexicographically.
+        //a == b = 0,   a < b = -1,   a > b = 1
+        int comparison = name.compareTo(current.name);
+        if(comparison < 0){
+            current.left = insert(current.left, name);
+        }
+        else if(comparison > 0){
+            current.right = insert(current.right, name);
+        }
+        return current;
+    }
+
+
+    public static void add(String name){
+        root = insert(root, name);
+        size++;
+    }
+
+    public static Node root(){
+        return root;
     }
 
     public static void main(String[] args){
 
-        Scanner scanner = new Scanner(System.in);
+        TreeDemo td = new TreeDemo();
         boolean finish = false;
-        String name = scanner.next();
-        root = new Node(name, null, null, null);
-
-
-        while(finish){
-            System.out.print("Enter name or enter blank to quit: ");
-            name = scanner.next();
-
-            if(name != null){
-                if(!checkValue(name)){
-                    insert(name);
-                    size++;
+        while(!finish){
+            System.out.print("Enter name or q to quit: ");
+            Scanner scanner = new Scanner(System.in);
+            String name = scanner.next();
+            if(checkValue(name)) {
+                if (name.equals("q")) {
+                    break;
                 }
+                add(name);
             }
             else{
-                finish = true;
+                System.out.println("Name already exists. Try again.");
             }
         }
+        System.out.println(td.size());
+        //td.inorder(root());
+        td.traverseInOrder(root());
     }
 
-    public void inOrder(Node node){
+    public void inorder(Node node){
         if(root != null){
-            inOrder(root.left);
-            System.out.println(root.getElement());
-            inOrder(root.right);
+            inorder(node.left);
+            System.out.println(node.getElement());
+            inorder(node.right);
         }
     }
 
-        //traversal		look at all of the traversals, it could be any of them. (probable pre/post/in order though)
+    public void traverseInOrder(Node node) {
+        if (node != null) {
+            traverseInOrder(node.left);
+            System.out.print(" " + node.name);
+            traverseInOrder(node.right);
+        }
+    }
+
+
+    //traversal		look at all of the traversals, it could be any of them. (probable pre/post/in order though)
 
 }
